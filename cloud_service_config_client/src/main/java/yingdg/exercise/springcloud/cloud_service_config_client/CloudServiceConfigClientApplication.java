174751ac796dfba2247af8d95ad0ad3f619acce8
YingDG <zdm.1991@outlook.com>
2017-10-26 14:05:46 +0800

    调整各模块pom，并依赖base pom

--- cloud_service_config_client/src/main/java/yingdg/exercise/springcloud/cloud_service_config_client/CloudServiceConfigClientApplication.java
+++ cloud_service_config_client/src/main/java/yingdg/exercise/springcloud/cloud_service_config_client/CloudServiceConfigClientApplication.java
@@ -1,11 +1,15 @@
 package yingdg.exercise.springcloud.cloud_service_config_client;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -16,7 +20,20 @@ public class CloudServiceConfigClientApplication {
         SpringApplication.run(CloudServiceConfigClientApplication.class, args);
     }
 
-    @Value("${foo}")
+    @Autowired
+    private Environment env;
+
+    @PostConstruct
+    public void initFoo() {
+        String foo = env.resolvePlaceholders("${foo}");
+        if (foo == null) {
+            foo = "foo version 3";
+        } else {
+            this.foo = foo;
+        }
+    }
+
+    // @Value("${foo}")
     private String foo;
 
     @GetMapping(value = "/config")
